1064ff3d83d8fbc79be61a019543674ce080e713
rykea2 <razershi@mailfence.com>
2025-01-24 11:04:29 +0100

    main

--- js/script.js
+++ js/script.js
@@ -1,6 +1,47 @@
-document.getElementById("myBtn").addEventListener("click", myFunction);
 
-function myFunction() {
-    document.getElementById("ButtonColorChange").classList.toggle("myStyle");
-    document.getElementById("ButtonColorChange").classList.toggle("pagestyle");
-}
+const TextPush = document.querySelector("#TextPush");
+const TextPop = document.querySelector("#TextPop");
+const TextUnshift = document.querySelector("#TextUnshift");
+const TextShift = document.querySelector("#TextShift");
+
+const TextList = [];
+
+TextPush.addEventListener("click", function () {
+    let inputBox = document.querySelector("#inputBox");
+    TextList.push(inputBox.value);
+
+    let TextDisplay = document.querySelector("#TextDisplay");
+    TextDisplay.textContent = TextList;
+
+    inputBox.value = "";
+});
+
+TextPop.addEventListener("click", function () {
+    let inputBox = document.querySelector("#inputBox");
+    TextList.pop(inputBox.value);
+
+    let TextDisplay = document.querySelector("#TextDisplay");
+    TextDisplay.textContent = TextList;
+
+    inputBox.value = "";
+});
+
+TextUnshift.addEventListener("click", function () {
+    let inputBox = document.querySelector("#inputBox");
+    TextList.unshift(inputBox.value);
+
+    let TextDisplay = document.querySelector("#TextDisplay");
+    TextDisplay.textContent = TextList;
+
+    inputBox.value = "";
+});
+
+TextShift.addEventListener("click", function () {
+    let inputBox = document.querySelector("#inputBox");
+    TextList.shift(inputBox.value);
+
+    let TextDisplay = document.querySelector("#TextDisplay");
+    TextDisplay.textContent = TextList;
+
+    inputBox.value = "";
+});
